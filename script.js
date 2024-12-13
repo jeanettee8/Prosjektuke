6ded199e53bcba57ed1c73d607fb38f39a83dca7
Jeanette <ekje112024@gmail.com>
2024-12-13 11:19:38 +0100

    fixed snowglobe

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// var button1 = document.getElementById("number1");
-// var button2 = document.getElementById("number2");
-// var button3 = document.getElementById("number3");
-// if (Unsure.checked) {
-//   radio == Naughty;
-// }
-
-// function "myFunction"()
-// alert("You have pressed the enter key, use submit button instead");
-// return false;
-
-// onkeydown="if(event.keyCode = 13)myFunction";
-
-// if (firstInput)==!""(return false;);
-
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
